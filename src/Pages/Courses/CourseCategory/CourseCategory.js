b330f6ddfee13ee38367a81e43f70efa8bb692e7
ShamimaNasrin <shamima.nasrion007@gmail.com>
2022-10-26 14:34:29 +0600

    project setup

---
+++ src/Pages/Courses/CourseCategory/CourseCategory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CourseCategory = () => {
+    return (
+        <div>
+            <h1>Course category</h1>
+        </div>
+    );
+};
+
+export default CourseCategory;
