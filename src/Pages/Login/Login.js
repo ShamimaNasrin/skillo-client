075ffb4828ecbec763d9c65ad0a991dcac015e46
ShamimaNasrin <shamima.nasrion007@gmail.com>
2022-10-27 23:38:13 +0600

    route fix

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -48,6 +48,7 @@ const Login = () => {
             .then(result => {
                 const user = result.user;
                 console.log(user);
+                navigate(from, { replace: true });
             })
             .catch(error => console.error(error))
     }
@@ -59,6 +60,7 @@ const Login = () => {
             .then(result => {
                 const user = result.user;
                 console.log(user);
+                navigate(from, { replace: true });
             })
             .catch(error => console.error(error))
     }
